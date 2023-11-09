e4bf42415a575f0b3eb45c9344481c93dbc683f8
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-11-09 18:19:51 +0200

    created a pike specific about page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,17 @@ import '../public/jquery.fancybox.css';
 import '../public/preview.css';
 import '../public/style.css';
 import './App.css'
-import AboutUs from './components/AboutUs/AboutUs';
+// import AboutUs from './components/AboutUs/AboutUs';
 import Footer from './components/Footer/Footer';
+// import Pike from './components/Species/Pike';
 
 function App() {
  
 
   return (
     <>
-      <AboutUs />
-
+      {/* <AboutUs /> */}
+      {/* <Pike /> */}
       <Footer />
     </>
   )
---
+++ client/src/components/Species/Pike.jsx
@@ -0,0 +1,18 @@
+const Pike = () => {
+    return (
+        <div className="pike-species-page">
+            <h2>Pike</h2>
+            <figure className="pike-img"><img src="img/pikeExplanation.jpg" alt="" /></figure>
+            <h1>Details</h1>
+            <p className="pike-info">
+                <span>
+                    The pike is an aggressive predatory fish found in most lakes an rivers in Europe and Northern America.
+                    It feeds on everything it sets its eyes, even ducks and rats. You can catch with all sorts of lures.
+                    Ir grows to 1.30m long and 25kg. The best lures for it are big soft baits and jerks. Some people also catch it on so called spoons.
+                </span>
+            </p>
+        </div>
+    )
+}
+
+export default Pike;
