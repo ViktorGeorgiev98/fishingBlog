931160299c7ac21aa27d3cbec37e81ce2db6cb60
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-11-09 17:47:16 +0200

    created Footer element and used it to test that React was initiated and everything is okay

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-
+import '../public/bootstrap.css';
+import '../public/grid.css'
+import '../public/ie.css'
+import '../public/jquery.fancybox.css';
+import '../public/preview.css';
+import '../public/style.css';
 import './App.css'
+import Footer from './components/Footer/Footer';
 
 function App() {
  
 
   return (
     <>
+
       
+      <Footer />
     </>
   )
 }
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>FISHING BLOG FOR REACT JS PROJECT EXAM.</p>
+        </footer>
+    )
+}
+
+export default Footer;
