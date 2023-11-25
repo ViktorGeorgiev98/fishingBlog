6e8af7a9e438e0847c4e4d342ebecd24eab9ea57
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-11-25 11:01:43 +0200

    implemented register logic

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -9,20 +9,36 @@ const Register = () => {
     const navigate = useNavigate();
 
 
-    const onSubmitRegisterHandler = (e) => {
+    const onSubmitRegisterHandler = async (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const username = formData.get('username');
         const email = formData.get('email');
         const password = formData.get('password');
         const rePassword = formData.get('rePassword');
-        if (!email || !password || !rePassword) {
+        if (!email || !password || !rePassword || !username) {
             return alert('All fields are mandatory!!!');
         }
         if (password !== rePassword) {
             return alert('Password and repeat password must be the same!!!');
         }
         console.log({username, email, password, rePassword});
+        try {
+          const response = await fetch("http://localhost:3030/users/register", {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({email, password, username})
+          })
+          if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+          } else {
+            throw new Error(response.statusText);
+          }
+        } catch(e) {
+          console.log(e.message);
+          return alert(e.message);
+        }
         navigate('/login')
     }
 
