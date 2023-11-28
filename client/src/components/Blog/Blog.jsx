d21ca5b55b39e33f24022cafc385204c8d6062cd
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-11-28 19:03:21 +0200

    started work on the blog page

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+
 const Blog = () => {
+    const [createBlog, setCreateBlog] = useState(false);
+
+
+
     return (
-        <h1>Blog</h1>
+        <div className="blog-page">
+            <h1>Blog</h1>
+            <h3>If you want to add a new article, please click the button
+                <button className="add-blog">Add article</button>
+            </h3>
+            {/* {setCreateBlog &&
+            
+            } */}
+        </div>
     )
 }
 
