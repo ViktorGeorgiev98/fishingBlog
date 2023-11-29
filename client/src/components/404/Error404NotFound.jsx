a2e57f50ef42a655f2df56cb86dbac004ed4b282
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-11-29 19:27:19 +0200

    error404 page changes

--- client/src/components/404/Error404NotFound.jsx
+++ client/src/components/404/Error404NotFound.jsx
@@ -1,25 +1,18 @@
 const Error404NotFound = () => {
     return (
-        <div id="wrapper">
-        <section className="container">
-          <div className="dynamicContent">
-            {/* Content */}
-            <div className="inner">
-              <div className="row">
-                <div className="span4 offset2">
-                  <img src="img/page404_pic1.png" className="margin3" alt="" />
-                </div>
-                <div className="span5">
-                  <h2 className="margin4">Sorry, page not found.</h2>
-                  <p>
-                    The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.
-                  </p>
-                </div>
+      <div id="errorPage">
+      <section className="error-container">
+        <div className="dynamicContent">
+          <div className="inner">
+            <div className="row">
+              <div className="image-container">
+                <img src="https://www.wpoven.com/blog/wp-content/uploads/2022/09/error-404.png" className="pulsate" alt="" />
               </div>
             </div>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
+    </div>
     )
 }   
 
