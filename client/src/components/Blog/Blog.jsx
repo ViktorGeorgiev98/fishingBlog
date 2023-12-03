e4eb02cc3b60af60e8e44c788f0195785bcd70e6
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-03 14:12:51 +0200

    adding blog only if authetnicated

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import CreateBlog from "./CreateBlog";
 import BlogListElement from "./BlogListElement";
 import BlogDetails from "./BlogDetails";
+import { useAuth } from "../../context/AuthProvider";
 
 const Blog = () => {
     const [createBlog, setCreateBlog] = useState(false);
     const [articles, setArticles] = useState([]);
     const [articleDetails, setArticleDetails] = useState(false);
+    const { isAuthenticated } = useAuth();
 
     useEffect(() => {
         fetch('http://localhost:3030/data/fishBlogArticles')
@@ -18,9 +20,11 @@ const Blog = () => {
     return (
         <div className="blog-page">
             <h1>Blog</h1>
-            <h3>If you want to add a new article, please click the button
+            {isAuthenticated() && 
+                <h3>If you want to add a new article, please click the button
                 <button className="add-blog" onClick={() => setCreateBlog(!createBlog)}>Add article</button>
-            </h3>
+                </h3>
+            }
             {createBlog  &&
                 <CreateBlog createBlog={createBlog} setCreateBlog={setCreateBlog} />
             }
