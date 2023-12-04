eeed6a86dd215c263bce495f39f18a09f3fce7fe
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-04 18:57:50 +0200

    blog list element css

--- client/src/components/Blog/BlogListElement.jsx
+++ client/src/components/Blog/BlogListElement.jsx
@@ -8,7 +8,7 @@ const BlogListElement = (props) => {
         <li>
             <img src={props.imageUrl} alt={props.shortDescription}></img>
             <p>Writer name: {props.writerName}</p>
-            <p>About {props.shortDescription}</p>
+            <p>About: {props.shortDescription}</p>
             <Link to={`/blog/${props.id}/details`}>
                 <button className="btn-btn-read-more">Read more</button>
             </Link>
